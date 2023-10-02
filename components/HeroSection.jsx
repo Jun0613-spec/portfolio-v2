9576d4a95e0b4896108802f4fbd130764f458a77
Jun0613-spec <kumo5110@gmail.com>
2023-10-02 16:08:37 +0100

    pic changed

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -12,11 +12,11 @@ const HeroSection = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src="/Jun.jpg"
+            src="/me.png"
             alt="me"
             width={360}
             height={360}
-            className="rounded-full shadow-2xl object-contain"
+            className="rounded-full shadow-2xl"
           />
         </div>
         <div className="md:mt-2 md:w-3/5">
